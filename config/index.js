e5e8af29f1ea3b213ccea49e4ca5beba697f5d3c
melvil <paradise-interactive@web.de>
2018-07-24 19:54:47 +0200

    prepared cordova build

--- config/index.js
+++ config/index.js
@@ -8,7 +8,7 @@ module.exports = {
   dev: {
     // Paths
     assetsSubDirectory: "static",
-    assetsPublicPath: "/",
+    assetsPublicPath: "",
     proxyTable: {},
 
     // Various Dev Server settings
@@ -41,7 +41,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, "../dist"),
     assetsSubDirectory: "static",
-    assetsPublicPath: "/",
+    assetsPublicPath: "",
 
     /**
      * Source Maps
