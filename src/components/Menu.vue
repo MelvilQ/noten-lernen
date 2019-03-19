0f66054f77747090d889f2b2221355cc0d050ca1
melvil <paradise-interactive@web.de>
2019-03-19 20:18:14 +0100

    added new translations to UI

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,123 +1,141 @@
 <template>
-	<div class="menu">
-		<div class="start-container">
-			<button class="start" @click="startGame">{{$t('START')}}</button>
-		</div>
-		<LastResultDisplay v-if="lastResult" :lastResult="lastResult" />
-		<StatisticsGraph v-if="statistics.lastScores.length >= 2" />
-		<div class="settings">
-			<h3>{{$t('settings')}}</h3>
-			<div class="setting">
-				<SelectOption :label="$t('ROUND_LENGTH')" :items="gameLengthOptions" v-model="options.gameLength" />
-			</div>
-			<div class="setting">
-				<SelectOption :label="$t('NOTE_INPUT')" :items="inputModeOptions" v-model="options.inputMode" />
-			</div>
-			<div class="setting">
-				<SelectOption :label="$t('CLEF')" :items="clefOptions" v-model="options.clef" />
-			</div>
-			<div class="setting">
-				<SelectOption :label="$t('DIFFICULTY')" :items="difficultyOptions" v-model="options.difficulty" />
-			</div>
-			<div class="setting">
-				<SelectOption :label="$t('ACCIDENTALS')" :items="accidentalOptions" v-model="options.accidentals" />
-			</div>
-			<div>
-				<SelectOption :label="$t('SOUND')" :items="soundOptions" v-model="options.sound" />
-			</div>
-			<div>
-				<SelectOption :label="$t('LANGUAGE')" :items="languageOptions" v-model="options.language" />
-			</div>
-		</div>
-	</div>
+  <div class="menu">
+    <div class="start-container">
+      <button class="start" @click="startGame">{{$t('START')}}</button>
+    </div>
+    <LastResultDisplay v-if="lastResult" :lastResult="lastResult"/>
+    <StatisticsGraph v-if="statistics.lastScores.length >= 2"/>
+    <div class="settings">
+      <h3>{{$t('settings')}}</h3>
+      <div class="setting">
+        <SelectOption
+          :label="$t('ROUND_LENGTH')"
+          :items="gameLengthOptions"
+          v-model="options.gameLength"
+        />
+      </div>
+      <div class="setting">
+        <SelectOption
+          :label="$t('NOTE_INPUT')"
+          :items="inputModeOptions"
+          v-model="options.inputMode"
+        />
+      </div>
+      <div class="setting">
+        <SelectOption :label="$t('CLEF')" :items="clefOptions" v-model="options.clef"/>
+      </div>
+      <div class="setting">
+        <SelectOption
+          :label="$t('DIFFICULTY')"
+          :items="difficultyOptions"
+          v-model="options.difficulty"
+        />
+      </div>
+      <div class="setting">
+        <SelectOption
+          :label="$t('ACCIDENTALS')"
+          :items="accidentalOptions"
+          v-model="options.accidentals"
+        />
+      </div>
+      <div>
+        <SelectOption :label="$t('SOUND')" :items="soundOptions" v-model="options.sound"/>
+      </div>
+      <div>
+        <SelectOption :label="$t('LANGUAGE')" :items="languageOptions" v-model="options.language"/>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import SelectOption from './SelectOption';
-import LastResultDisplay from './LastResultDisplay';
-import StatisticsGraph from './StatisticsGraph';
+import SelectOption from "./SelectOption";
+import LastResultDisplay from "./LastResultDisplay";
+import StatisticsGraph from "./StatisticsGraph";
 
-import Options from '../model/Options';
-import Statistics from '../model/Statistics';
+import Options from "../model/Options";
+import Statistics from "../model/Statistics";
 
 export default {
-	name: 'Menu',
-	components: { SelectOption, LastResultDisplay, StatisticsGraph },
-	props: {
-		lastResult: Object
-	},
-	data(){
-		return {
-			options: Options,
-			statistics: Statistics
-		}
-	},
-	computed: {
-		gameLengthOptions(){
-			return [
-				{value: 20, label: this.$t('twentySeconds')}, 
-				{value: 60, label: this.$t('oneMinute')}, 
-				{value: 300, label: this.$t('fiveMinutes')}
-			];
-		},
-		inputModeOptions(){
-			return [
-				{value: 'button', label: this.$t('buttons')},
-				{value: 'keyboard', label: this.$t('piano')}
-			];
-		},
-		clefOptions() {
-			return [
-				{value: 'treble', label: this.$t('trebleClef')},
-				{value: 'bass', label: this.$t('bassClef')},
-				{value: 'all', label: this.$t('both')}
-			];
-		},
-		difficultyOptions() {
-			return [
-				{value: 'easy', label: this.$t('easy')},
-				{value: 'normal', label: this.$t('normal')},
-				{value: 'hard', label: this.$t('hard')}
-			];
-		},
-		accidentalOptions() {
-			return [
-				{value: 'no', label: this.$t('none')},
-				{value: 'onlySharp', label: this.$t('sharp')},
-				{value: 'onlyFlat', label: this.$t('flat')},
-				{value: 'sharpAndFlat', label: this.$t('sharpAndFlat')}
-			];
-		},
-		soundOptions(){
-			return [
-				{value: false, label: this.$t('off')},
-				{value: true, label: this.$t('on')}
-			];
-		},
-		languageOptions() {
-			return [
-				{value: 'en', label: 'EN'},
-				{value: 'de', label: 'DE'},
-				{value: 'pl', label: 'PL'}
-			];
-		}
-	},
-	watch: {
-		'options.language': function(lang){
-			this.$i18n.locale = lang;
-		}
-	},
-	methods: {
-		startGame(){
-			Options.saveOptions();
-			this.$emit('startGame');
-		}
-	},
-	created(){
-		Options.loadOptions();
-		Statistics.loadStatistics();
-	}
-}
+  name: "Menu",
+  components: { SelectOption, LastResultDisplay, StatisticsGraph },
+  props: {
+    lastResult: Object
+  },
+  data() {
+    return {
+      options: Options,
+      statistics: Statistics
+    };
+  },
+  computed: {
+    gameLengthOptions() {
+      return [
+        { value: 20, label: this.$t("twentySeconds") },
+        { value: 60, label: this.$t("oneMinute") },
+        { value: 300, label: this.$t("fiveMinutes") }
+      ];
+    },
+    inputModeOptions() {
+      return [
+        { value: "button", label: this.$t("buttons") },
+        { value: "keyboard", label: this.$t("piano") }
+      ];
+    },
+    clefOptions() {
+      return [
+        { value: "treble", label: this.$t("trebleClef") },
+        { value: "bass", label: this.$t("bassClef") },
+        { value: "all", label: this.$t("both") }
+      ];
+    },
+    difficultyOptions() {
+      return [
+        { value: "easy", label: this.$t("easy") },
+        { value: "normal", label: this.$t("normal") },
+        { value: "hard", label: this.$t("hard") }
+      ];
+    },
+    accidentalOptions() {
+      return [
+        { value: "no", label: this.$t("none") },
+        { value: "onlySharp", label: this.$t("sharp") },
+        { value: "onlyFlat", label: this.$t("flat") },
+        { value: "sharpAndFlat", label: this.$t("sharpAndFlat") }
+      ];
+    },
+    soundOptions() {
+      return [
+        { value: false, label: this.$t("off") },
+        { value: true, label: this.$t("on") }
+      ];
+    },
+    languageOptions() {
+      return [
+        { value: "en", label: "EN" },
+        { value: "de", label: "DE" },
+        { value: "fr", label: "FR" },
+        { value: "pl", label: "PL" },
+        { value: "ln", label: "LN" }
+      ];
+    }
+  },
+  watch: {
+    "options.language": function(lang) {
+      this.$i18n.locale = lang;
+    }
+  },
+  methods: {
+    startGame() {
+      Options.saveOptions();
+      this.$emit("startGame");
+    }
+  },
+  created() {
+    Options.loadOptions();
+    Statistics.loadStatistics();
+  }
+};
 </script>
 
 <style scoped>
--- src/resources/strings.js
+++ src/resources/strings.js
@@ -1,5 +1,7 @@
 import de from "./de";
 import en from "./en";
+import fr from "./fr";
 import pl from "./pl";
+import ln from "./ln";
 
-export default { de, en, pl };
+export default { de, en, fr, pl, ln };
