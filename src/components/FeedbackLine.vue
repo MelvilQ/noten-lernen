4774384f6d470dec52b6262aab872f78f5c6a879
Code Monet <92645652+code-monet@users.noreply.github.com>
2022-09-08 06:58:33 -0700

    Feedback note and correct/wrong on the same line.
    This should help with vertical space esp. in landscape orientation on small screens

--- src/components/FeedbackLine.vue
+++ src/components/FeedbackLine.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="feedback-line">
-    <span class="feedback correct" v-if="visibleFeedback === 'correct'">{{$t('correctExcl')}}</span>
+    <span class="feedback correct" v-if="visibleFeedback === 'correct'">{{$t('correctExcl')}}: {{ $t(feedbackNote) }}</span>
     <span class="feedback wrong" v-if="visibleFeedback === 'wrong'">{{$t('wrongExcl')}}</span>
     <span class="feedback dummy" v-if="visibleFeedback === 'none'">dummy</span>
     <span class="feedback dummy" v-if="visibleFeedback === 'none_temp'">dummy_temp</span>
@@ -22,6 +22,7 @@ export default {
   },
   props: {
     feedback: String,
+    feedbackNote: String,
     uniqueId: Number
   },
   watch: {
